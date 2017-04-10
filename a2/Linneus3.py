3def543a2fd75ecb2c84a11d2ece0317fefcf062
grahamaloo <graham.thomas.kelly@gmail.com>
2017-04-09 22:39:54 -0700

    finished part I

--- a2/Linneus3.py
+++ a2/Linneus3.py
@@ -1,3 +1,15 @@
+'''
+PartII.py
+Graham Kelly, CSE 415, Spring 2017, University of Washington
+Instructor:  S. Tanimoto.
+Assignment 2 Part II.  ISA Hierarchy Manipulation
+
+Status of the implementation of new features:
+
+All forms of redundancy detection and processing are working.
+# Extra credit (Cycle detection and processing) implemented and working.
+'''
+
 # Linneus3.py
 # Implements storage and inference on an ISA hierarchy
 # This Python program goes with the book "The Elements of Artificial
@@ -17,23 +29,24 @@ from re import *   # Loads the regular expression module.
 ISA = {}
 INCLUDES = {}
 ARTICLES = {}
+DEBUG = True #TODO: change for prod.
 
 def store_isa_fact(category1, category2):
-    'Stores one fact of the form A BIRD IS AN ANIMAL'
+    '''Stores one fact of the form A BIRD IS AN ANIMAL'''
     # That is, a member of CATEGORY1 is a member of CATEGORY2
-    try :
+    try:
         c1list = ISA[category1]
         c1list.append(category2)
-    except KeyError :
+    except KeyError:
         ISA[category1] = [category2]
-    try :
+    try:
         c2list = INCLUDES[category2]
         c2list.append(category1)
-    except KeyError :
+    except KeyError:
         INCLUDES[category2] = [category1]
         
 def get_isa_list(category1):
-    'Retrieves any existing list of things that CATEGORY1 is a'
+    '''Retrieves any existing list of things that CATEGORY1 is a'''
     try:
         c1list = ISA[category1]
         return c1list
@@ -49,17 +62,22 @@ def get_includes_list(category1):
         return []
     
 def isa_test1(category1, category2):
-    'Returns True if category 2 is (directly) on the list for category 1.'
+    '''Returns True if category 2 is (directly) on the list for category 1.'''
     c1list = get_isa_list(category1)
     return c1list.__contains__(category2)
     
-def isa_test(category1, category2, depth_limit = 10):
-    'Returns True if category 1 is a subset of category 2 within depth_limit levels'
-    if category1 == category2 : return True
-    if isa_test1(category1, category2) : return True
+def isa_test(category1, category2, depth_limit = 10, verbose = False):
+    '''Returns True if category 1 is a subset of category 2 within depth_limit levels'''
+    if category1 == category2:
+        if verbose: print('You don\'t have to tell me that.')
+        return True
+    if isa_test1(category1, category2):
+        if verbose: print('You already told me that.')
+        return True
     if depth_limit < 2 : return False
     for intermediate_category in get_isa_list(category1):
         if isa_test(intermediate_category, category2, depth_limit - 1):
+            if verbose: print('You don\'t have to tell me that.')
             return True
     return False
 
@@ -81,8 +99,12 @@ def linneus():
     print('For example, you could tell me "An ant is an insect."')
     while True :
         info = input('Enter an ISA fact, or "bye" here: ')
-        if info == 'bye': return 'Goodbye now!'
-        process(info)
+        if info == 'bye': 
+            return 'Goodbye now!'
+        elif DEBUG and info == 'printtree': #useful for debugging
+            print(ISA)
+        else:
+            process(info)
 
 # Some regular expressions used to parse the user sentences:    
 assertion_pattern = compile(r"^(a|an|A|An)\s+([-\w]+)\s+is\s+(a|an)\s+([-\w]+)(\.|\!)*$", IGNORECASE)    
@@ -91,17 +113,18 @@ what_pattern = compile(r"^What\s+is\s+(a|an)\s+([-\w]+)(\?\.)*", IGNORECASE)
 why_pattern = compile(r"^Why\s+is\s+(a|an)\s+([-\w]+)\s+(a|an)\s+([-\w]+)(\?\.)*", IGNORECASE)    
 
 def process(info) :
-    'Handles the user sentence, matching and responding.'
+    '''Handles the user sentence, matching and responding.'''
     result_match_object = assertion_pattern.match(info)
-    if result_match_object != None :
+    if result_match_object != None:
         items = result_match_object.groups()
-        store_article(items[1], items[0])
-        store_article(items[3], items[2])
-        store_isa_fact(items[1], items[3])
-        print("I understand.")
+        if not isa_test(items[1], items[3], verbose = True):
+            store_article(items[1], items[0])
+            store_article(items[3], items[2])
+            store_isa_fact(items[1], items[3])
+            print("I understand.")
         return
     result_match_object = query_pattern.match(info)
-    if result_match_object != None :
+    if result_match_object != None:
         items = result_match_object.groups()
         answer = isa_test(items[1], items[3])
         if answer :
@@ -154,7 +177,7 @@ def answer_why(x, y):
 
 from functools import reduce
 def report_chain(x, y):
-    'Returns a phrase that describes a chain of facts.'
+    '''Returns a phrase that describes a chain of facts.'''
     chain = find_chain(x, y)
     all_but_last = chain[0:-1]
     last_link = chain[-1]
@@ -164,7 +187,7 @@ def report_chain(x, y):
     return main_phrase + new_last_phrase
 
 def report_link(link):
-    'Returns a phrase that describes one fact.'
+    '''Returns a phrase that describes one fact.'''
     x = link[0]
     y = link[1]
     a1 = get_article(x)
@@ -172,7 +195,7 @@ def report_link(link):
     return a1 + " " + x + " is " + a2 + " " + y + ", "
     
 def find_chain(x, z):
-    'Returns a list of lists, which each sublist representing a link.'
+    '''Returns a list of lists, which each sublist representing a link.'''
     if isa_test1(x, z):
         return [[x, z]]
     else:
